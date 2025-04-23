6440b6af8e2b414d2f6b3973e6351e2fd5a83c19
Your Name <you@example.com>
2025-04-22 22:27:51 -0400

    initialisation de la classe controler

---
+++ controler/controler.py
@@ -0,0 +1,10 @@
+
+class Controler:
+    "Gère les interations entre la vue et le modele"
+    
+    def __init__(self,platine):
+        self.platine = platine
+        
+        
+    def debuter(self):
+        pass
